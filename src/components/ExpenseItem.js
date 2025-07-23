8578c8a0301af39587b61064990e6f2433cf9253
DHURANTHAM007 <anjosh753@gmail.com>
2025-07-24 00:32:54 +0530

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { TiDelete } from 'react-icons/ti';
-const ExpenseItem = ({ id, name, cost, handleDeleteExpense }) => 
-{
-  return 
-  (
-    <li className='list-group-item d-flex justify-content-between align-items-center'>
-    {name}
-    <div>
-    <span className='badge badge-primary rounded-pill me-3'>
-    ${cost}
-  </span>
-  <TiDelete size='1.5em' onClick={() => handleDeleteExpense(id)} style={{cursor: 'pointer'}}></TiDelete>
-  </div>
-  </li>
- );
+
+const ExpenseItem = ({ id, name, cost, handleDeleteExpense }) => {
+    return (
+        <li className='list-group-item d-flex justify-content-between align-items-center'>
+            <strong>{name}</strong>
+            <div>
+                <span className='badge badge-primary rounded-pill me-3'>
+                    ${cost}
+                </span>
+                <TiDelete size='1.5em' onClick={() => handleDeleteExpense(id)} style={{cursor: 'pointer'}}></TiDelete>
+            </div>
+        </li>
+    );
 };
+
 export default ExpenseItem;
