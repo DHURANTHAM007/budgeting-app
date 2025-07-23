128512a2a1d4abfc667524e0e01bf1d2d2d90c17
DHURANTHAM007 <anjosh753@gmail.com>
2025-07-24 00:16:59 +0530

    Update AddExpenseForm.js

--- src/components/AddExpenseForm.js
+++ src/components/AddExpenseForm.js
@@ -1,55 +1,56 @@
 import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
-const AddExpenseForm = ({ onAddExpense }) => {
-  const [name, setName] = useState('');
-  const [cost, setCost] = useState('');
+const AddExpenseForm = ({ handleAddExpense }) => {
+    const [name, setName] = useState('');
+    const [cost, setCost] = useState('');
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (name && cost) {
-      const newExpense = {
-        id: uuidv4(),
-        name,
-        cost: parseInt(cost),
-      };
-      onAddExpense(newExpense);
-      setName('');
-      setCost('');
-    }
-  };
+    const onSubmit = (event) => {
+        event.preventDefault();
+        const expense = {
+            id: uuidv4(),
+            name: name,
+            cost: parseInt(cost),
+        };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <div className='row'>
-        <div className='col'>
-          <label>Name</label>
-          <input
-            type='text'
-            className='form-control'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </div>
-        <div className='col'>
-          <label>Cost</label>
-          <input
-            type='number'
-            className='form-control'
-            value={cost}
-            onChange={(e) => setCost(e.target.value)}
-            required
-          />
-        </div>
-      </div>
-      <div className='row mt-3'>
-        <div className='col'>
-          <button type='submit' className='btn'>Save</button>
-        </div>
-      </div>
-    </form>
-  );
+        handleAddExpense(expense);
+        setName('');
+        setCost('');
+    };
+
+    return (
+        <form onSubmit={onSubmit}>
+            <div className='row'>
+                <div className='col-sm'>
+                    <label>Name</label>
+                    <input
+                        required='required'
+                        type='text'
+                        className='form-control'
+                        value={name}
+                        onChange={(event) => setName(event.target.value)}
+                    ></input>
+                </div>
+                <div className='col-sm'>
+                    <label>Cost</label>
+                    <input
+                        required='required'
+                        type='number'
+                        className='form-control'
+                        value={cost}
+                        onChange={(event) => setCost(event.target.value)}
+                    ></input>
+                </div>
+            </div>
+            <div className='row mt-3'>
+                <div className='col-sm'>
+                    <button type='submit' className='btn btn-primary'>
+                        Save
+                    </button>
+                </div>
+            </div>
+        </form>
+    );
 };
 
 export default AddExpenseForm;
